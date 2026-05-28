d7db76962ad8743e55da3aa91b4257b9863d75ff
Johan Sebastian Florez Ospina <sebasflorez69@gmail.com>
2026-05-28 16:45:27 -0500

    refactor: visualización de datos modificada

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
+import os
 import time
 
 from cloud import MQTTClient
 from sensor import Sensor
-from sensor_types import humedad, presion, temperatura
+from sensor_types import (
+    calidad_aire,
+    co2,
+    humedad,
+    luz,
+    presion,
+    ruido,
+    temperatura,
+)
 
 
 def main() -> None:
@@ -10,20 +19,45 @@ def main() -> None:
     sensor_hum = Sensor("HumeSensor", "Humedad", humedad)
     sensor_press = Sensor("PressSensor", "Presión", presion)
 
-    cloud_1 = MQTTClient(node="nodo_1")
-    cloud_2 = MQTTClient(node="nodo_2")
+    cloud_1 = MQTTClient(
+        node="nodo_1",
+        broker=mqtt_broker,
+        port=mqtt_port,
+        topic_prefix=mqtt_prefix,
+    )
+    cloud_2 = MQTTClient(
+        node="nodo_2",
+        broker=mqtt_broker,
+        port=mqtt_port,
+        topic_prefix=mqtt_prefix,
+    )
+
+    readings = [
+        (cloud_1, Sensor("TempSensor", "Temperatura", temperatura)),
+        (cloud_1, Sensor("HumeSensor", "Humedad", humedad)),
+        (cloud_1, Sensor("LightSensor", "Luz", luz)),
+        (cloud_1, Sensor("AQISensor", "Calidad de Aire", calidad_aire)),
+        (cloud_2, Sensor("PressSensor", "Presión", presion)),
+        (cloud_2, Sensor("NoiseSensor", "Ruido", ruido)),
+        (cloud_2, Sensor("CO2Sensor", "CO2", co2)),
+    ]
 
     try:
         while True:
             print("- Sensores ", "-" * 38)
-            print(sensor_temp)
-            print(sensor_hum)
-            print(sensor_press)
+            batch = []
+
+            for cloud, sensor in readings:
+                valor = sensor.get_data()
+                batch.append((cloud, sensor, valor))
+                print(
+                    f"{cloud.node} | {sensor.name} - SENSOR_{sensor.node_number} | {sensor.data_type}: {valor}"
+                )
+
             print("-" * 50)
 
-            cloud_1.send(sensor_temp, sensor_temp.get_data())
-            cloud_1.send(sensor_hum, sensor_hum.get_data())
-            cloud_2.send(sensor_press, sensor_press.get_data())
+            for cloud, sensor, valor in batch:
+                cloud.send(sensor, valor)
 
             time.sleep(1)
     except KeyboardInterrupt:
